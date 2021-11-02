9e023b7cbdfedc4799d8ca3d5ca63f9c30b5c2d7
StephhyL <Stephanie Li>
2021-11-02 13:49:18 -0400

    spinner1.js one full circle

---
+++ spinner1.js
@@ -0,0 +1,45 @@
+
+setTimeout(() => {
+  process.stdout.write('\r|      ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/       ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-       ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\       ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|       ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/       ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-       ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r\\       ');
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r|       \n');
+}, 1700);
+
+
+//process.stdout.write("hello from spinner1.js... \rheyyy\n");
+
+// \r brings the cruser back to the front
+// \n brings the cruser onto a new line
+
+// "hello from spinner1.js ... \rheyyy" // heyyy%
+// "hello from spinner1.js ... \rheyyy\n" // heyyy from spinner1.js // %
